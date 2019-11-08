21980b2685cb073229fdb4ac1467832ee20009dd
BuildTools <unconfigured@null.spigotmc.org>
2019-11-08 16:54:00 +0100

    renamed

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Singelton
+
+    public static Player instace;
+
+    void Awake()
+    {
+        instace = this;
+    }
+
+    #endregion;
+
+    public PlayerStats stats;
+    public CharacterCombat combat;
+    public GameObject player;
+
+    void Start()
+    {
+        stats.OnHealthIsZero += Die;
+    }
+
+    void Die()
+    {
+        //TODO: Playe Die 
+    }
+}
