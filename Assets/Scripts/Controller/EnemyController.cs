fee7d1a52214140675a818d4b9ba01573dd08957
BuildTools <unconfigured@null.spigotmc.org>
2019-11-08 12:43:20 +0100

    moved

---
+++ Assets/Scripts/Controller/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float lookRadius = 10f;
+    Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        target = PlayerManager.instace.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
