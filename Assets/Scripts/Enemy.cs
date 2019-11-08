799ec48458b09b4ffc582118a42f7af54395ca5a
BuildTools <unconfigured@null.spigotmc.org>
2019-11-08 16:55:19 +0100

    add Enemy class
    implements the Interactable interface

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterStats))]
+public class Enemy : Interactable
+{
+    CharacterStats stats;
+
+    void Start()
+    {
+        stats = GetComponent<CharacterStats>();
+        stats.OnHealthIsZero += Die;
+    }
+
+    public override void Interact()
+    {
+        CharacterCombat combat = Player.instace.combat;
+        combat.Attack(stats);
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
