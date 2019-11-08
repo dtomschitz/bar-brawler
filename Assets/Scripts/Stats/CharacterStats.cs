e2eb6b2a21cd7f8ac264139963858cbc28b45240
BuildTools <unconfigured@null.spigotmc.org>
2019-11-08 12:43:40 +0100

    add stats management classes

---
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+
+    public int maxHealth;
+    public int damage;
+
+    public int currentHealth { get; protected set; }
+    public event System.Action OnHealthIsZero;
+
+    public virtual void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public virtual void Start()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //Make sure damage doesn't go below 0;
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            if (OnHealthIsZero != null)
+            {
+                OnHealthIsZero();
+            }
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+}
