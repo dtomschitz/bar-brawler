862dd36adb9a1be470e59683eec297ca639587ca
dt035 <dt035@hdm-stuttgart.de>
2019-11-11 18:03:05 +0100

    add basic inventory system

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+
+    new public string name = "Neues Item";
+    public Sprite icon = null;
+
+    public virtual void Use() { }
+}
