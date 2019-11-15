7902a14cef6ebc4eebcd4a7548fa9124e120cb5c
dt035 <dt035@hdm-stuttgart.de>
2019-11-15 18:34:13 +0100

    added class to pick up collectable items
    Collected items are automatically added to the inventory

---
+++ Assets/Scripts/Entities/Inventory/Collectable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : Interactable
+{
+    public UsableItem item;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Interact();
+            Inventory.instance.AddItem(item);
+            Destroy(gameObject);
+        }
+    }
+}
