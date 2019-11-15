068978cf9b62fbc54883e3dee0d4fa5774ebc001
dt035 <dt035@hdm-stuttgart.de>
2019-11-15 09:35:30 +0100

    moved

--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -6,21 +6,31 @@ using UnityEngine;
 public class Enemy : Interactable
 {
     public CharacterStats stats;
+    public GameObject DamagePopup;
 
     void Start()
     {
         stats = GetComponent<CharacterStats>();
-        stats.OnHealthIsZero += Die;
+        stats.OnHealthIsZero += Death;
     }
 
     public override void Interact()
     {
+        if (stats.isDead()) return;
+
         EntityCombat combat = Player.instace.combat;
-        Debug.Log(combat);
         combat.Attack(stats);
+
+        if (DamagePopup) ShowDamagePopup();
     }
 
-    void Die()
+    void ShowDamagePopup()
+    {
+        var popup = Instantiate(DamagePopup, transform.position, Quaternion.identity, transform);
+        popup.GetComponent<TextMesh>().text = stats.currentHealth.ToString();
+    }
+
+    void Death()
     {
         Destroy(gameObject);
     }
