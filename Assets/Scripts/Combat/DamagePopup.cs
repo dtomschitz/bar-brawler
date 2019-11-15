e1dda8e66ac730f05c5b26e6741e12dae11397b9
dt035 <dt035@hdm-stuttgart.de>
2019-11-15 09:34:00 +0100

    added basic damage popup text

---
+++ Assets/Scripts/Combat/DamagePopup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePopup : MonoBehaviour
+{
+    public float DestroyTime = 3f;
+    public Vector3 Offset = new Vector3(0, 2, 0);
+    public Vector3 Intensity = new Vector3(0.5f, 0, 0);
+
+    void Start()
+    {
+        Destroy(gameObject, DestroyTime);
+        transform.localPosition += Offset;
+        transform.localPosition += new Vector3(Random.Range(-Intensity.x, Intensity.x), Random.Range(-Intensity.y, Intensity.y), Random.Range(-Intensity.z, Intensity.z));
+    }
+}
---
+++ Assets/Scripts/Combat/EntityCombat.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterStats))]
+public class EntityCombat : MonoBehaviour
+{
+
+    public float attackRate = 1f;
+    public event System.Action OnAttack;
+
+    CharacterStats playerStats;
+    CharacterStats enemyStats;
+
+    void Start()
+    {
+        playerStats = GetComponent<CharacterStats>();
+    }
+
+    void Update()
+    {
+        // attackCooldown -= Time.deltaTime;
+    }
+
+    public void Attack(CharacterStats stats)
+    {
+        this.enemyStats = stats;
+        StartCoroutine(DoDamge(stats, .15f));
+
+
+        if (OnAttack != null)
+        {
+            OnAttack();
+        }
+    }
+
+    IEnumerator DoDamge(CharacterStats stats, float delay)
+    {
+        Debug.Log(transform.name + " damage: " + playerStats.damage);
+        enemyStats.TakeDamage(playerStats.damage);
+        yield return new WaitForSeconds(delay);
+    }
+}
