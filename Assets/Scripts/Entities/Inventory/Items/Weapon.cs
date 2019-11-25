c169b80b78a029f05ac2ae26c546dcf86b315be9
dt035 <dt035@hdm-stuttgart.de>
2019-11-25 23:57:55 +0100

    added weapon item

---
+++ Assets/Scripts/Entities/Inventory/Items/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Neue Waffe", menuName = "Inventory/Weapon")]
+public class Weapon : UsableItem
+{
+    public int damageModifier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
