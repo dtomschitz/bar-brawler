b248366af3cb967163a992216527d6239ed5d8d3
dt035 <dt035@hdm-stuttgart.de>
2019-12-06 11:11:00 +0100

    added fist item in order to split game logic of the item from the player controller

---
+++ Assets/Scripts/Inventory/Items/Fist.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fist : Weapon
+{
+    private EntityCombat combat;
+    private PlayerAnimator animator;
+
+    void Start()
+    {
+        combat = Player.instance.combat;
+        animator = Player.instance.animator;
+    }
+
+    public override void OnInteract()
+    {
+        StartCoroutine(AttackRoutine());
+        animator.OnAttack();
+    }
+
+    private IEnumerator AttackRoutine()
+    {
+        combat.state = CombatState.ATTACKING;
+        yield return new WaitForSeconds(1f);
+        combat.state = CombatState.IDLE;
+    }
+}
