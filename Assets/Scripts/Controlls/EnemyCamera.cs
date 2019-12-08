1ce4dc571b20fe368c927673e8d40f5a9ecddbb5
Duane <de030@hdm-stuttgart.de>
2019-12-08 14:56:54 +0100

    refreshed commit after my changes vanished

---
+++ Assets/Scripts/Controlls/EnemyCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCamera : MonoBehaviour
+{
+    public Camera enemyCamera;
+    void Update()
+    {
+        transform.LookAt(transform.position + enemyCamera.transform.rotation * Vector3.back,
+                        enemyCamera.transform.rotation * Vector3.down);
+    }
+}
