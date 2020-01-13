de87475e6cc431b46039abd79e33868e54df21ec
dt035 <dt035@hdm-stuttgart.de>
2020-01-13 13:00:46 +0100

    add animated doors

---
+++ Assets/Scripts/Wavesystem/SpawnPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public Door door;
+
+    public void OpenDoor()
+    {
+        if (door) door.OpenDoor();
+    }
+
+    public void CloseDoor()
+    {
+        if (door) door.CloseDoor();
+    }
+}
---
+++ Assets/Scripts/Wavesystem/Wave.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    public int number;
+    public int enemiesCount;
+
+}
