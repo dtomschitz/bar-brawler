503f8ff9038881509574c9cf85ac43d779214e45
Duane <de030@hdm-stuttgart.de>
2020-01-15 15:17:15 +0100

    edited Scene in Main Menu, Buttons now works

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public string levelToLoad = "MainScene";
+
+    public void Play()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exciting...");
+        Application.Quit();
+    }   
+}
