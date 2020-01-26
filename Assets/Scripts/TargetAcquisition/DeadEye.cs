4d466fa6c949914cd8b16b123d2ecda583e0ace1
dt035 <dt035@hdm-stuttgart.de>
2020-01-26 16:04:09 +0100

    update main scene

---
+++ Assets/Scripts/TargetAcquisition/DeadEye.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using Utils;
+
+public static class DeadEye {
+
+    private const int TAG_AMOUNT_MAX = 5;
+
+    private static PostProcessVolume blackWhitePostProcessVolume;
+    private static bool deadEyeActive;
+    private static List<DeadEyeTag> deadEyeTagList;
+    private static Action<List<Vector3>> onDeadEyeEnd;
+
+    public static void Init(PostProcessVolume blackWhitePostProcessVolume) {
+        DeadEye.blackWhitePostProcessVolume = blackWhitePostProcessVolume;
+        deadEyeActive = false;
+        deadEyeTagList = new List<DeadEyeTag>();
+    }
+
+    public static void Start(Action<List<Vector3>> onDeadEyeEnd) {
+        DeadEye.onDeadEyeEnd = onDeadEyeEnd;
+        deadEyeTagList.Clear();
+        AnimateBlackWhiteEffect(0f, 1f, 1f);
+        AnimateTimeScaleEffect(1f, .15f, .6f);
+        deadEyeActive = true;
+    }
+
+    public static void End() {
+        AnimateBlackWhiteEffect(1f, 0f, 1f);
+        AnimateTimeScaleEffect(.15f, 1f, .8f);
+        deadEyeActive = false;
+        DeadEyeUI.DestroyAllTags();
+        List<Vector3> tagPositionList = new List<Vector3>();
+        foreach (DeadEyeTag deadEyeTag in deadEyeTagList) {
+            tagPositionList.Add(deadEyeTag.GetPosition());
+        }
+        onDeadEyeEnd(tagPositionList);
+    }
+
+    public static bool IsActive() {
+        return deadEyeActive;
+    }
+
+    public static void TryAddTag(Func<Vector3> tagPositionFunc) {
+        if (deadEyeTagList.Count < TAG_AMOUNT_MAX) {
+            DeadEyeTag deadEyeTag = new DeadEyeTag(tagPositionFunc);
+            deadEyeTagList.Add(deadEyeTag);
+            DeadEyeUI.AddTag_Static(deadEyeTag);
+        }
+    }
+
+    private static void AnimateBlackWhiteEffect(float start, float end, float animateTime) {
+        float time = 0f;
+        FunctionUpdater.Create(() => {
+            time += Time.unscaledDeltaTime / animateTime;
+            blackWhitePostProcessVolume.weight = Mathf.Lerp(start, end, time);
+            return time >= 1f;
+        });
+    }
+    
+    private static void AnimateTimeScaleEffect(float start, float end, float animateTime) {
+        float time = 0f;
+        FunctionUpdater.Create(() => {
+            time += Time.unscaledDeltaTime / animateTime;
+            Time.timeScale = Mathf.Lerp(start, end, time);
+            return time >= 1f;
+        });
+    }
+
+
+    public class DeadEyeTag {
+
+        private Func<Vector3> tagPositionFunc;
+
+        public DeadEyeTag(Func<Vector3> tagPositionFunc) {
+            this.tagPositionFunc = tagPositionFunc;
+        }
+
+        public Vector3 GetPosition() {
+            return tagPositionFunc();
+        }
+    }
+}
---
+++ Assets/Scripts/TargetAcquisition/DeadEyeUI.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadEyeUI : MonoBehaviour {
+
+    private static DeadEyeUI instance;
+
+    [SerializeField] private Camera uiCamera;
+    private Transform tagTemplateTransform;
+    private List<DeadEyeTagUIObject> deadEyeTagUIObjectList;
+
+    private void Awake() {
+        instance = this;
+        tagTemplateTransform = transform.Find("tagTemplateTransform");
+        tagTemplateTransform.gameObject.SetActive(false);
+        deadEyeTagUIObjectList = new List<DeadEyeTagUIObject>();
+    }
+
+    public static void DestroyAllTags() {
+        foreach (DeadEyeTagUIObject deadEyeTagUIObject in instance.deadEyeTagUIObjectList) {
+            deadEyeTagUIObject.DestroySelf();
+        }
+        instance.deadEyeTagUIObjectList.Clear();
+    }
+
+    public static void AddTag_Static(DeadEye.DeadEyeTag deadEyeTag) {
+        instance.AddTag(deadEyeTag);
+    }
+
+    private void AddTag(DeadEye.DeadEyeTag deadEyeTag) {
+        Transform tagTransform = Instantiate(tagTemplateTransform, transform);
+        tagTransform.gameObject.SetActive(true);
+        DeadEyeTagUIObject deadEyeTagUIObject = new DeadEyeTagUIObject(deadEyeTag, tagTransform, uiCamera);
+        deadEyeTagUIObjectList.Add(deadEyeTagUIObject);
+    }
+
+    private void Update() {
+        foreach (DeadEyeTagUIObject deadEyeTagUIObject in deadEyeTagUIObjectList) {
+            deadEyeTagUIObject.Update();
+        }
+    }
+
+    private class DeadEyeTagUIObject {
+
+        private DeadEye.DeadEyeTag deadEyeTag;
+        private Transform transform;
+        private Camera uiCamera;
+
+        public DeadEyeTagUIObject(DeadEye.DeadEyeTag deadEyeTag, Transform transform, Camera uiCamera) {
+            this.deadEyeTag = deadEyeTag;
+            this.transform = transform;
+            this.uiCamera = uiCamera;
+            Update();
+        }
+
+        public void Update() {
+            Vector3 tagWorldPosition = deadEyeTag.GetPosition();
+            Vector3 screenPoint = Camera.main.WorldToScreenPoint(tagWorldPosition);
+            Vector3 uiWorldPoint = uiCamera.ScreenToWorldPoint(screenPoint);
+            Vector3 uiPoint = transform.parent.InverseTransformPoint(uiWorldPoint);
+            uiPoint.z = 0f;
+            transform.localPosition = uiPoint;
+        }
+
+        public void DestroySelf() {
+            Destroy(transform.gameObject);
+        }
+    }
+
+}
