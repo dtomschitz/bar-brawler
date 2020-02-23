46324f7688a3de80202332e95e8926400ddf6e10
dt035 <dt035@hdm-stuttgart.de>
2020-02-23 09:18:24 +0100

    remove unused code

--- Assets/Scripts/Wave/SpawnPoint.cs
+++ Assets/Scripts/Wave/SpawnPoint.cs
@@ -4,18 +4,6 @@ namespace Wave
 {
     public class SpawnPoint : MonoBehaviour
     {
-        public Door door;
-
-        public void OpenDoor()
-        {
-            if (door) door.OpenDoor();
-        }
-
-        public void CloseDoor()
-        {
-            if (door) door.CloseDoor();
-        }
-
         public Vector3 Position
         {
             get { return transform.position; }
